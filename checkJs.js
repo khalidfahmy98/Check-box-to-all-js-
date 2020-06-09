1b7c66ce5840ae877ec94146ed7955df7d65f674
Khalid Fahmy <53705950+khalidfahmy98@users.noreply.github.com>
2020-06-09 09:46:37 +0200

    Update checkJs.js

--- checkJs.js
+++ checkJs.js
@@ -1,19 +1,2 @@
-    function checkAllFunction(parent, children) {
-        $(parent).change(function () {
-            if ($(this).is(':checked')) {
-                $(children).each(function () {
-                    if (!$(this).hasClass("checkedChek")) {
-                        $(this).attr("checked", "checked");
-                        $(this).addClass("checkedChek");
-                    }
-                });
-            } else {
-                $(children).each(function () {
-                    if ($(this).hasClass("checkedChek")) {
-                        $(this).removeClass("checkedChek");
-                        $(this).prop('checked', false);
-                    }
-                });
-            }
-        });
-    }
+    function checkAllFunction(parent, children) {$(parent).change(function () {if ($(this).is(':checked')) {$(children).each(function () {if (!$(this).hasClass("checkedChek")) {$(this).attr("checked", "checked");$(this).addClass("checkedChek");}});} else {$(children).each(function () {if ($(this).hasClass("checkedChek")) {$(this).removeClass("checkedChek");$(this).prop('checked', false);}});} });}
+                    
